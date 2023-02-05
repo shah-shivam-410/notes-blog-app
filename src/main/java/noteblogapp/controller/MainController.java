e4707350b4229da1ed15992304ca810873095749
shah-shivam-410 <shah.shivam.410@gmail.com>
2023-02-05 21:15:37 +0530

    Initial setup for MVC application.

---
+++ src/main/java/noteblogapp/controller/MainController.java
@@ -0,0 +1,19 @@
+package noteblogapp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@GetMapping(path = "/")
+	public String home() {
+		logger.info("Endpoint invoked: {}, Method Invoked: {}", "/", "home");
+		return "index";
+	}
+	
+}
